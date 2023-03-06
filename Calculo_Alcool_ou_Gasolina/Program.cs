dd18a32aac0391eb306f62235de31e51b77e06f4
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-03-05 22:54:18 -0300

    Add project files.

---
+++ Calculo_Alcool_ou_Gasolina/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double alcool;
+        double gasolina;
+
+        Console.Write("Informe o valor do Álcool: R$ ");
+        alcool = double.Parse(Console.ReadLine());
+        Console.Write("Informe o valor do Gasolina: R$ ");
+        gasolina = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        Console.WriteLine("A relação entre o Álcool e a Gasolina é de " + (alcool / gasolina).ToString("F2"));
+        Console.WriteLine();
+
+        if ((alcool / gasolina) <= 0.72)
+        {
+            Console.WriteLine("O Álcool é mais vanjatoso");
+        }
+        else
+        {
+            Console.WriteLine("A Gasolina é mais vanjatosa");
+        }
+    }
+}
